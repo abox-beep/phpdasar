144cbc2da5dc87b1423ea967d8b12ebdcac763d1
Sultoni Rahman <sultonirahman85@gmail.com>
2022-08-02 19:45:04 +0700

    menambahkan folder pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,57 @@
+<?php
+//Sintaks PHP Dasar
+//TAG PHP
+//Sintaks Komentar
+//Ini adalah komentar
+/*
+ini juga adalah komentar
+*/
+
+//Pertemuan 2 - Sintaks PHP
+
+//Standar Output
+//echo, print
+//print_r
+//var_dump
+
+//Penulisan sintaks PHP
+// 1. PHP didalam HTML
+// 2. HTML didalam PHP
+
+//Variabel dan tipe data
+//Variabel = untuk menampung sebuah nilai, tidak menambahkan tipe data dari nilai
+//Tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "SultoniR";
+// echo "Halo, Nama Saya $nama";
+
+//Operator
+//Aritmatika
+// + - * / %
+// echo 1 + 1;
+
+//Penggabung String = Concatenation / concat
+//.
+// $nama_depan = "Sultoni";
+// $nama_belakang = "Rahman";
+
+// echo $nama_depan. ' ' . $nama_belakang;
+
+//Assignment = Penugasan
+//=, +=, -=, /=, %=, .=, *=
+
+//Perbandingan
+//<, >, <=, >=, ==, !=
+
+// var_dump(1 === "1");
+
+//Identitas
+//===, !==
+
+//Logika
+//&*, ||, !
+
+$x = 20;
+var_dump( $x<20 && $x%2==0 );
+
+
+?>
